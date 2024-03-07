c60f3131db2d08dff0071118238f5b95ff3ea876
SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2024-03-07 12:49:00 +0700

    fetch without server directory

--- composables/FetchStore.ts
+++ composables/FetchStore.ts
@@ -6,15 +6,29 @@ const useFetchProducts = () => {
   const isUnauthorized = ref(false);
 
   const fetchProducts = async () => {
-    try {
-      const response = await fetch('/api/proxy');
-      if (response.ok) {
-        products.value = await response.json();
-      } 
+    try 
+    {
+      const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJzdWIiOjEsImlhdCI6MTcwOTYyNDA1MSwiZXhwIjozMzI0NTYyNDA1MX0.01BroMNk9JXcaluf4IJ1HuZeCfDGAmxB5lgjhTFUOqE'
+  
+      const response = await fetch('http://wallserver.dyndns.info:10000/product/all', {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      });
+  
+      if (response.status === 401) {
+        return 'Unauthorized';
+      } else if (!response.ok) {
+        return { error: response.statusText };
+      }
+  
+      const data = await response.json();
+      products.value =  data.message;
+  
     } catch (error) {
       isUnauthorized.value = true;
     }
-    
+
   };
 
   onMounted(fetchProducts);
